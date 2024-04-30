d08f18ad3c6bafb35d3bfff96462e6385c0e8f56
Eduardo Jiménez <eduardojilo@outlook.com>
2024-04-29 23:21:52 -0600

    Move basic test content from top to test_suit_1

--- tb/aquilesv_tb.cpp
+++ tb/aquilesv_tb.cpp
@@ -11,27 +11,7 @@
 //Tests suites
 #include "test_suite_1.cpp"
 
-#define MAX_SIM_TIME 20
-vluint64_t sim_time = 0;
-
 int main(int argc, char** argv, char** env) {
-    Valu *dut = new Valu;
-
     test_suite_1();
-
-    Verilated::traceEverOn(true);
-    VerilatedVcdC *m_trace = new VerilatedVcdC;
-    dut->trace(m_trace, 5);
-    m_trace->open("test_results/waveform.vcd");
-
-    while (sim_time < MAX_SIM_TIME) {
-        dut->clk ^= 1;
-        dut->eval();
-        m_trace->dump(sim_time);
-        sim_time++;
-    }
-
-    m_trace->close();
-    delete dut;
     exit(EXIT_SUCCESS);
 }
--- tb/test_suite_1.cpp
+++ tb/test_suite_1.cpp
@@ -1,12 +1,48 @@
 #include <stdlib.h>
 #include <iostream>
 #include <cstdio>
+#include <random>
 #include <thread>
 
+#define MAX_SIM_TIME 20
 int basic_alu_test()
 {
+    std::random_device seed;
+    std::mt19937 gen{seed()}; // seed the generator
+    std::uniform_int_distribution<> dist{0, 5}; // Number of functions supported by ALU
+    int funct_num;
+    vluint64_t sim_time = 0;
+    Valu *dut = new Valu;
     freopen("test_results/test_suite_1/basic_alu_test/basic_alu_test.log", "w", stdout);
     printf("Starting basic_alu_test...");
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *m_trace = new VerilatedVcdC;
+    dut->trace(m_trace, 5);
+    m_trace->open("test_results/test_suite_1/basic_alu_test/waveform.vcd");
+    dut->clk ^= 1;
+    dut->op_in = 0;
+    dut->a_in = 0;
+    dut->b_in = 0;
+    dut->eval();
+    m_trace->dump(sim_time);
+    sim_time++;
+    //TODO: Need to implement init and run phases
+    while (sim_time < MAX_SIM_TIME) {
+        dut->clk ^= 1;
+        funct_num = dist(gen);
+        //dut->op_in = dist(gen);     //Pick a random function from ALU and pass it to op type input
+        dut->op_in = 0;     //Pick a random function from ALU and pass it to op type input
+        //Leverage from random number generator to randomize inputs
+        //dut->a_in = dist(gen);
+        //dut->b_in = dist(gen);
+        dut->a_in = 2;
+        dut->b_in = 1;
+        dut->eval();
+        m_trace->dump(sim_time);
+        sim_time++;
+    }
+    m_trace->close();
+    delete dut;
     fclose(stdout);
     return 0;
 }
